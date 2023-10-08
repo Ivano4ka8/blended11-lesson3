6ae9e08e7a71af44f75641b447bf653eab0863e4
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-10-08 12:54:18 +0300

    redux_async

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,41 @@
 import { Container, Header, SearchForm, Section, Text } from 'components';
-import { useSelector } from 'react-redux';
-import { selectTodos } from 'redux/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectTodos,
+  selectLoading,
+  selectError,
+  selectFilter,
+} from 'redux/selectors';
 import TodoList from 'components/TodoList/TodoList';
+import { useEffect } from 'react';
+import { fetchTodos } from 'redux/operations';
+import { setFilter } from 'redux/filterSlice';
 
 export const App = () => {
   const todos = useSelector(selectTodos);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  const filter = useSelector(selectFilter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
 
   return (
     <>
       <Header />
       <Section>
         <Container>
+          <input
+            type="text"
+            name="filter"
+            value={filter}
+            onChange={e => dispatch(setFilter(e.target.value))}
+          />
           <SearchForm />
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
           {todos.length === 0 && (
             <Text textAlign="center">There are no any todos ... </Text>
           )}
--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -1,22 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchTodos, addTodo, deleteTodo } from './operations';
 
 const todosSlice = createSlice({
   // Имя слайса
   name: 'todos',
   // Начальное состояние редюсера слайса
-  initialState: { items: [] },
-  // Объект редюсеров
-  reducers: {
-    addTodo(state, { payload }) {
-      state.items.push(payload);
+  initialState: { items: [], loading: false, error: null },
+  extraReducers: {
+    //fetchAll
+    [fetchTodos.pending](state) {
+      state.loading = true;
     },
-    deleteTodo(state, { payload }) {
-      state.items = state.items.filter(item => item.id !== payload);
+    [fetchTodos.fulfilled](state, action) {
+      state.loading = false;
+      state.items = action.payload;
+    },
+    [fetchTodos.rejected](state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    //addTodo
+    [addTodo.pending](state) {
+      state.loading = true;
+    },
+    [addTodo.fulfilled](state, action) {
+      state.loading = false;
+      state.items.push(action.payload);
+    },
+    [addTodo.rejected](state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    //deleteTodo
+    [deleteTodo.pending](state) {
+      state.loading = true;
+    },
+    [deleteTodo.fulfilled](state, action) {
+      state.loading = false;
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+    [deleteTodo.rejected](state, action) {
+      state.loading = false;
+      state.error = action.payload;
     },
   },
 });
 
-// Генераторы экшенов
-export const { addTodo, deleteTodo } = todosSlice.actions;
 // Редюсер слайса
 export const todosReducer = todosSlice.reducer;
